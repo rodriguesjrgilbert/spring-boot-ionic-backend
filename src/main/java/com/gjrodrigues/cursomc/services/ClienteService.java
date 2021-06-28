3914f983ccf7718b2ac6432bbe8fa89656a5feec
Gilberto Junior Rodrigues <gilberto.junior@outlook.com>
2021-06-28 15:30:24 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/gjrodrigues/cursomc/services/ClienteService.java
+++ src/main/java/com/gjrodrigues/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que esteja com entidade relacionada.");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados.");
 		}
 	}
 
